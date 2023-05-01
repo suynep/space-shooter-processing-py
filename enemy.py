230c4537c90b1e32f09d8c3d2f057ccd54de00cd
suynep <suy.nepal@gmail.com>
2023-05-01 15:17:27 +0545

    nice

--- enemy.py
+++ enemy.py
@@ -1,13 +1,8 @@
-<<<<<<< HEAD
-class Enemy():
-    rot_fac = PI/2
-=======
 
 
 class Enemy():
     rot_fac = PI/2
     a_sp = 5
->>>>>>> 52a318c (added bullet movement)
     def __init__(self, p_img, a_img, posx, posy, ammo):
         self.p_img = p_img
         self.a_img = a_img
@@ -30,9 +25,7 @@ class Enemy():
         rotate(Enemy.rot_fac)
         image(self.a_img, 0, 0)
         popMatrix()
-<<<<<<< HEAD
-=======
+
         self.ammopos.x -= Enemy.a_sp
->>>>>>> 52a318c (added bullet movement)
     
     
